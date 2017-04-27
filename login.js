f0e958346d3358f191919cc3557c73dcd27e2ce5
sebastian <estradis_91@hotmail.com>
2017-04-27 14:30:35 +0200

    edits to js file

--- login.js
+++ login.js
@@ -5,23 +5,21 @@ function logInGoogle() {
   .then(function(result) {
 	if( firebase.auth().currentUser.providerData[0].displayName === null ) {
 		 showUser.innerHTML=firebase.auth().currentUser.providerData[0].email;
-		 secBtn.disabled=false;
+		 
 		 
 	 } else {
 		  showUser.innerHTML=firebase.auth().currentUser.providerData[0].displayName;
-		  secBtn.disabled=false;
+		  
 	 }
 	  
 	let user = result.user;
 	console.log(user);
-	showImg.src=firebase.auth().currentUser.providerData[0].photoURL;
 	});
 	}
 //   logout function from quizzaro
 function logOutUser() {
 	firebase.auth().signOut()
     .then(function(result) {
-	console.log("signed out!")
 	secBtn.disabled=true;
 	secImg.src="";
   })
@@ -35,6 +33,7 @@ function logOutUser() {
   let showContent =document.getElementById('show-container');
   let signInBtn = document.getElementById('loginBtn');
   let signOutBtn = document.getElementById('signoutBtn');
+  let showUser = document.getElementById('showUser');
 
   signInBtn.addEventListener('click', function(event) {
 	  logInGoogle();
