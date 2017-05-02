b3948742133c10b051cabc560ccd6da149d08e9e
nauberinho <nauber91@hotmail.com>
2017-05-02 14:40:46 +0200

    changed niklasreact.jsx

--- niklasreact.jsx
+++ niklasreact.jsx
@@ -1,17 +1,17 @@
-
 class CountryComponent extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+        questions: [],
         currentCountry: 'No Country',
-        currentCountryPopulation: 0
+        currentCountryPopulation: 3
     }
-
-      this.getCountry = this.getCountry.bind(this);
-      this.getCountryPopulation = this.getCountryPopulation.bind(this);
-      this.getCountryAndPopulation = this.getCountryAndPopulation.bind(this);
-      this.setCurrentCountry = this.setCurrentCountry.bind(this);
-      this.setCurrentCountryPopulation = this.setCurrentCountryPopulation.bind(this);
+    this.getCountry = this.getCountry.bind(this);
+    this.getCountryPopulation = this.getCountryPopulation.bind(this);
+    this.setCurrentCountry = this.setCurrentCountry.bind(this);
+    this.setCurrentCountryPopulation = this.setCurrentCountryPopulation.bind(this);
+    this.returnQuestionAndAnswers = this.returnQuestionAndAnswers.bind(this);
+    this.simulateQuestions=this.simulateQuestions.bind(this);
   }
 
 getCountry(){
@@ -27,12 +27,28 @@ getCountry(){
         console.log('3', country)
         let randomNumber = Math.floor(Math.random() * 236) + 1;
         country = json.countries[randomNumber];
-        console.log(country)
-        this.setCurrentCountry(country)
-        this.getCountryPopulation()
+        console.log(country);
+        this.setCurrentCountry(country);
+        this.getCountryPopulation();
     })
 }
 
+    getCountryPopulation(){
+    console.log(this.state.currentPopulation)
+    let population;
+    let url = 'http://api.population.io:80/1.0/population/' + this.state.currentCountry +  '/2015-12-24'
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json)
+        population = json['total_population'].population;
+        this.setCurrentCountryPopulation(population);
+        this.returnQuestionAndAnswers();
+    });
+}
+    
 setCurrentCountry(country){
     this.setState({currentCountry: country})
 }
@@ -40,47 +56,44 @@ setCurrentCountry(country){
 setCurrentCountryPopulation(population){
     this.setState({currentCountryPopulation: population})
 }
-      
-getCountryPopulation(){
-    console.log(this.state.currentPopulation)
-    let population;
-    let url = 'http://api.population.io:80/1.0/population/' + this.state.currentCountry +  '/2015-12-24'
-    fetch(url)
-    .then(response => {
-        return response.json();  // Promise som kommer returnera JSON
-    })
-    .then(json => {  // objekt
-        console.log(json)
-        population = json['total_population'].population;
-        this.setCurrentCountryPopulation(population);
-    });
+   
+returnQuestionAndAnswers(){
+    let randomNumber = Math.floor(Math.random() * this.state.currentCountryPopulation/2) + this.state.currentCountryPopulation/3;
+    let num = this.state.currentCountryPopulation - randomNumber;
+    let numberToString = num.toString().split('');
+    let questionObject = {
+        text: "What's the population of " + this.state.currentCountry + '?',
+        a1: this.state.currentCountryPopulation, numberToString,
+        a2: this.state.currentCountryPopulation - randomNumber,
+        a3: this.state.currentCountryPopulation + randomNumber   
+    }
+    let allQuestions = this.state.questions;
+    allQuestions.push(questionObject);
+    this.setState({questions: allQuestions});
+
 }
     
-getCountryAndPopulation(){     
-this.getCountry();        
+// --------- OUR MAIN FUNCTION FOR SIMULATING QUESTIONS AND LIFT THEM TO STATE.------//
+simulateQuestions(){
+    for(let i=1; i<=10; i++){
+        this.getCountry();  
+    };
 }
       
 render(){
 return(
-
 <div>
-<div>{this.state.currentCountry}</div>
-<div>{this.state.currentCountryPopulation}</div>
-   <button onClick={this.getCountry}></button>
-   <button onClick={this.getCountryPopulation}></button>
-   <button onClick={this.getCountryAndPopulation}></button>
-    
+    <div>{this.state.currentCountry}</div>
+    <div>{this.state.currentCountryPopulation}</div>
+    <button onClick={this.simulateQuestions}></button>    
 </div>
 )
 
 }
 }
 
-
 ReactDOM.render(
-
-    <CountryComponent />, document.getElementById('app')
-
+<CountryComponent />, document.getElementById('app');
 );
 
 
