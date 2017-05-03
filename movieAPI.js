ae44156c82422547f9a360a50e93d1532d13f849
Rikard <rikard.mattsson@gmail.com>
2017-05-03 10:23:43 +0200

    movie api

--- movieAPI.js
+++ movieAPI.js
@@ -1,5 +1,6 @@
 var movies = {
   request: function(){
+    /*
     let data = "{}";
     let xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
@@ -11,6 +12,14 @@ var movies = {
     xhr.open("GET", "https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=8f9cb0455f3b5cefb95acc1c35525622");
     xhr.send(data);
     console.log("Request sent");
+*/
+    fetch("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=8f9cb0455f3b5cefb95acc1c35525622").then(function(response){
+      response.json().then(function(object){
+          console.log(object);
+      }).catch(function(error){
+          console.log("Network error");
+      });
+    });
   }
 }
 
