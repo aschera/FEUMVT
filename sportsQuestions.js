feb6f29314ce97ee1ebb6db03e7e173ec1a8faa1
sebastian <estradis_91@hotmail.com>
2017-05-05 13:09:51 +0200

    api

--- sportsQuestions.js
+++ sportsQuestions.js
@@ -2,7 +2,7 @@ var sports = {
 
     request: function() {
     let url = "http://api.football-data.org/v1/competitions/"; 
-    //url +="5524a123b5a54565a7f6d6caf6b6ac4b";
+    //url +="5524a123b5a54565a7f6d6caf6b6ac4b"; /* api key not needed */
     fetch(url).then(function(response){
       response.json().then(function(object){
           console.log(object);
