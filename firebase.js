f41d38e00f84952476f2cbd0370c9c62a89c1703
Rikard <rikard.mattsson@gmail.com>
2017-05-10 18:03:11 +0200

    spelling error

--- firebase.js
+++ firebase.js
@@ -92,7 +92,7 @@ var hs = {
     table.appendChild(hrow);
 
     let filteredList = hs.filterList(list);
-    filterList.forEach(hi =>{
+    filteredList.forEach(hi =>{
       let row = document.createElement("tr");
       let td1 = document.createElement("td");
       let td2 = document.createElement("td");
