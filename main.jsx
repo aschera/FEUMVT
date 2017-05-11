620481198aa52cecac8b134e5b67ea5aef7e1eb9
aschera <aschera@gmail.com>
2017-05-11 18:39:08 +0200

    21

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 			this.state = {
-          w: '',
+                    w: '310',
 					selected: '',
 					questions: [{
 							text: "",
